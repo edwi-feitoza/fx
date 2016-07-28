9cf85b779c7e5857fd880388ea9bc5e79d76f204
Edwi Feitoza <edwi.slacker@gmail.com>
2016-07-28 08:17:35 -0300

    Organização das variáveis que controlam o acesso ao banco de dados

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -5,17 +5,17 @@ var user = process.env.RDS_USERNAME;
 var password = process.env.RDS_PASSWORD;
 var dbPort = process.env.RDS_PORT;
 var connectionStringRDS = 'postgres://' + user + ':' + password + '@' + host + ':' + dbPort + '/' + database;
-//var connectionString = 'postgres://nodejs:nodejsapp@10.0.0.10:5432/desafio_fdex';
+var connectionStringLocal = 'postgres://nodejs:nodejsapp@10.0.0.10:5432/desafio_fdex';
 var connectionStringTest = 'postgres://nodejs:nodejsapp@10.0.0.10:5432/desafio_fdex_test';
 
 function createPgClient(){
-	console.log(connectionStringRDS);
-	if(!process.env.NODE_ENV || process.env.NODE_ENV == 'dev'){
-		return new pg.Client(connectionStringRDS);
-	}
 
-	if(process.env.NODE_ENV == 'test'){
+	if(process.env.NODE_ENV == 'dev'){
+		return new pg.Client(connectionStringLocal);
+	} else if(process.env.NODE_ENV == 'test'){
 		return new pg.Client(connectionStringTest);
+	} else {
+		return new pg.Client(connectionStringRDS);
 	}
 }
 
