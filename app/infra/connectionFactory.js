cca25cf15adc99d095a7c424a8c9a96181ef50f0
Edwi Feitoza <edwi.slacker@gmail.com>
2016-07-28 08:04:37 -0300

    Modificações no core para rodar no AWS Elastic Beanstalk

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,10 +1,17 @@
 var pg = require('pg');
-var connectionString = 'postgres://nodejs:nodejsapp@10.0.0.10:5432/desafio_fdex';
+var host = process.env.RDS_HOSTNAME;
+var database = 'desafio_fdex';
+var user = process.env.RDS_USERNAME;
+var password = process.env.RDS_PASSWORD;
+var dbPort = process.env.RDS_PORT;
+var connectionStringRDS = 'postgres://' + user + ':' + password + '@' + host + ':' + dbPort + '/' + database;
+//var connectionString = 'postgres://nodejs:nodejsapp@10.0.0.10:5432/desafio_fdex';
 var connectionStringTest = 'postgres://nodejs:nodejsapp@10.0.0.10:5432/desafio_fdex_test';
 
 function createPgClient(){
+	console.log(connectionStringRDS);
 	if(!process.env.NODE_ENV || process.env.NODE_ENV == 'dev'){
-		return new pg.Client(connectionString);
+		return new pg.Client(connectionStringRDS);
 	}
 
 	if(process.env.NODE_ENV == 'test'){
